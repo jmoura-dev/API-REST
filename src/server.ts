7c01ed3c3bd6ced86ddd5b043f4f7bb19dbb1ce3
Jackson <jackson1712@hotmail.com>
2023-04-22 13:39:34 -0300

    refactored items

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,21 @@
-import fastify from "fastify";
+import fastify from 'fastify'
+import { knex } from './database'
+import { env } from './env'
 
-const app = fastify();
+const app = fastify()
 
-app.get("/hello", () => {
-  return "Hello world"
+app.get('/hello', async () => {
+  const transactions = await knex('transactions')
+    .where('amount', 1000)
+    .select('*')
+
+  return transactions
 })
 
-app.listen({
-  port: 3333
-}).then(() => {
-  console.log(`Server is running on port: ${3333}`)
-});
+app
+  .listen({
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log(`Server is running on port: ${env.PORT}`)
+  })
